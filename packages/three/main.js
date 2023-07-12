8317b4836a6d93a8d06b39029b26f39c7b751e59
dewei.ma <xnmageis@outlook.com>
2023-07-12 22:36:03 +0800

    chore: three.js

---
+++ packages/three/main.js
@@ -0,0 +1,17 @@
+/**
+ * name: main.js
+ * author: Deve
+ * date: 2023-07-12
+ */
+
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+console.log(THREE, GLTFLoader, scene, camera, renderer)
