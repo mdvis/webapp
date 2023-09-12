531dba38f2c17722cfaa1e7a3200427e9e4d96d8
dewei.ma <xnmageis@outlook.com>
2023-09-12 18:30:54 +0800

    feat: demo

---
+++ packages/demo/babel.config.js
@@ -0,0 +1,23 @@
+/**
+ * name: babel.config.js
+ * author: Deve
+ * date: 2023-09-12
+ */
+
+const presets = [
+  [
+    "@babel/preset-env",
+    {
+      targets: {
+        edge: "17",
+        firefox: "60",
+        chrome: "67",
+        safari: "11.1",
+      },
+      useBuiltIns: "usage",
+      corejs: "3.6.4",
+    },
+  ],
+];
+
+module.exports = { presets };
